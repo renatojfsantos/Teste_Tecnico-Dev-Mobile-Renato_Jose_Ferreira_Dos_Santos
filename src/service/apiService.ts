1140e551546cac6ef30282b2abcac46d38e3b565
Renato Santos <renato_jfs@hotmail.com>
2021-05-01 23:36:39 -0300

    refactor: add new fetch getQuestions

--- src/service/apiService.ts
+++ src/service/apiService.ts
@@ -1,3 +1,5 @@
+import { DifficultyType } from '~/models/questionModel';
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const getCategories = async () => {
   try {
@@ -6,5 +8,20 @@ export const getCategories = async () => {
   } catch (error) {
     console.log('error', error);
     return [];
+  }
+};
+
+export const getQuestions = async (
+  categoryId: number,
+  difficulty: DifficultyType,
+) => {
+  try {
+    const result = await fetch(
+      `https://opentdb.com/api.php?amount=1&category=${categoryId}&difficulty=${difficulty}&type=multiple`,
+    );
+    return await result.json();
+  } catch (error) {
+    console.log('error', error);
+    return null;
   }
 };
