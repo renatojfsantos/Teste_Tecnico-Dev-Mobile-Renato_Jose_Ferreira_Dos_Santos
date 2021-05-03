069d1fee7d926119b6580d1834c0997e9f9674da
Renato Santos <renato_jfs@hotmail.com>
2021-05-02 22:45:04 -0300

    chore: add search functionality and issue presentation

--- src/views/Questions/index.tsx
+++ src/views/Questions/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { Children, useCallback, useEffect, useState } from 'react';
 import { View, ScrollView, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useApp } from '~/contexts/App';
@@ -17,58 +17,65 @@ import Question, {
 
 import palette from '~/theme/palette';
 import { Container } from './styles';
+import ActionModal from '~/components/ActionModal';
+import Text from '~/components/Text';
 
 const Questions: React.FC = () => {
   const { state, handle } = useApp();
   const navigation = useNavigation();
 
   const [answers, setAnswers] = useState<string[]>([]);
-  const [historyAnswers, setHistoryAnswers] = useState<HistoryAnswers>({
-    easy: { correct: 0, incorrect: 0 },
-    medium: { correct: 0, incorrect: 0 },
-    hard: { correct: 0, incorrect: 0 },
-  });
+  const [historyAnswers, setHistoryAnswers] = useState<HistoryAnswers>(
+    new HistoryAnswers(),
+  );
   const [controlLevel, setControlLevel] = useState<ControlLevelType>('start');
   const [selectAnswer, setSelectAnswer] = useState('');
   const [currentQuestion, setCurrentQuestion] = useState<Question | null>(null);
   const [totalQuestion, setTotalQuestion] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
-  const searchQuestion = useCallback(async () => {
-    const { selectedCategoryId, difficulty } = state;
-    const question = await getQuestionsController(
-      selectedCategoryId,
-      difficulty,
-    );
-    if (question) {
-      const resultAnswers = shuffleAnswerController(
-        question.correctAnswer,
-        question.incorrectAnswers,
+  const searchQuestion = useCallback(
+    async (difficultyType: DifficultyType) => {
+      const question = await getQuestionsController(
+        state.selectedCategoryId,
+        difficultyType,
       );
-      setAnswers(resultAnswers);
-      setCurrentQuestion(question);
-    }
-  }, [state]);
+      if (question) {
+        const resultAnswers = shuffleAnswerController(
+          question.correctAnswer,
+          question.incorrectAnswers,
+        );
+        setAnswers(resultAnswers);
+        setCurrentQuestion(question);
+        console.log('RESPOSTA CORRETA>>>>>', question.correctAnswer);
+      }
+    },
+    [state.selectedCategoryId],
+  );
 
   useEffect(() => {
-    searchQuestion();
+    searchQuestion('easy');
   }, [searchQuestion]);
 
   const handleAnswer = (answer: string) => {
     setSelectAnswer(answer);
   };
 
+  // const showAlertHistory =() => {
+  // }
+
   const handleSubmit = () => {
-    if (currentQuestion) {
+    if (currentQuestion && totalQuestion <= 9) {
       const isCorrect = selectAnswer === currentQuestion.correctAnswer;
       if (isCorrect) {
         const itemHistory = historyAnswers[state.difficulty];
         itemHistory.correct += 1;
-        setHistoryAnswers({ ...historyAnswers });
+        setHistoryAnswers(new HistoryAnswers({ ...historyAnswers }));
         setControlLevel(controlLevel === 'correct' ? 'start' : 'correct');
       } else {
         const itemHistory = historyAnswers[state.difficulty];
         itemHistory.incorrect += 1;
-        setHistoryAnswers({ ...historyAnswers });
+        setHistoryAnswers(new HistoryAnswers({ ...historyAnswers }));
         setControlLevel(controlLevel === 'incorrect' ? 'start' : 'incorrect');
       }
 
@@ -81,19 +88,22 @@ const Questions: React.FC = () => {
           state.difficulty === 'hard' ? 'medium' : 'easy';
         handle.setDifficulty(nextLevel);
       }
-      if (totalQuestion <= 9) {
-        searchQuestion();
-      } else {
-        setTotalQuestion(totalQuestion + 1);
-        // mostra alerta com historico de resultados
-      }
+      searchQuestion(state.difficulty);
+      setTotalQuestion(totalQuestion + 1);
+      setShowModal(false);
     }
+    setSelectAnswer('');
+  };
+
+  const confirmAnswer = () => {
+    setShowModal(true);
   };
 
   const renderMenu = () => {
     return (
       <View>
         <FlatList
+          ListHeaderComponent={<Text>{currentQuestion?.question}</Text>}
           keyExtractor={item => item}
           data={answers}
           renderItem={({ item }) => (
@@ -109,17 +119,50 @@ const Questions: React.FC = () => {
     );
   };
 
+  const renderModal = () => {
+    let title = '';
+    let containDetails = '';
+    if (totalQuestion > 9) {
+      const sumCorrect = historyAnswers.getCorrectsCount();
+      const sumIncorrect = historyAnswers.getIncorrectsCount();
+      title += `Você acertou ${sumCorrect} perguntas`;
+      title += `Você errou ${sumIncorrect} perguntas`;
+      containDetails += `Fácil: ${historyAnswers.easy.correct} acerto e ${historyAnswers.easy.incorrect} erro`;
+      containDetails += `Médio: ${historyAnswers.medium.correct} acerto e ${historyAnswers.medium.incorrect} erro`;
+      containDetails += `Difícil: ${historyAnswers.hard.correct} acerto e ${historyAnswers.hard.incorrect} erro`;
+    } else if (currentQuestion?.correctAnswer === selectAnswer) {
+      title = 'Você acertou!';
+      containDetails = 'Colocar icone depois!!!';
+    } else {
+      title = 'Você errou!';
+      containDetails = 'Colocar icone depois!!!';
+    }
+    return (
+      <ActionModal title={title} children={<Text>{containDetails}</Text>} />
+    );
+  };
+
   return (
     <Container>
       <Header title="Dev Mobile" />
       <>
         <View>
           {renderMenu()}
-          <Button
-            style={{ backgroundColor: palette.whiteTransparent }}
-            title="Resposta"
-            handleClick={() => handleSubmit()}
-          />
+          {showModal && renderModal()}
+          {!!selectAnswer && (
+            <Button
+              style={{ backgroundColor: palette.whiteTransparent }}
+              title={showModal ? 'Avançar' : 'Responder'}
+              handleClick={() => {
+                if (showModal) {
+                  if (totalQuestion > 9) {
+                    navigation.goBack();
+                  }
+                  handleSubmit();
+                } else confirmAnswer();
+              }}
+            />
+          )}
         </View>
       </>
     </Container>
